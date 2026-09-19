b974ac3118e6a064bea617fe7972579f842cb186
Agustin Bompadre <agustin.bompadre@sap.com>
2026-09-18 21:21:58 -0700

    Sign release APK with consistent keystore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,9 +16,18 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = System.getenv("SIGNING_STORE_FILE")?.let { file(it) }
+            storePassword = System.getenv("SIGNING_STORE_PASSWORD")
+            keyAlias = System.getenv("SIGNING_KEY_ALIAS")
+            keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
